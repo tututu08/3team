736376cef407a90742c1a4c53c60abe63f73f315
choiyeonggeon <aws8792@naver.com>
2025-03-25 11:40:14 +0900

    test11

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by 최영건 on 3/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
